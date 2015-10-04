101bd460607e6eb66d87aab4360a140ed8d11903
jnims <jmnims@gmail.com>
2015-10-03 22:56:43 -0400

    Sync

--- FOS-SaveEditor/Utility/VaultDataInterface.cs
+++ FOS-SaveEditor/Utility/VaultDataInterface.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace FOS_SaveEditor.Utility
